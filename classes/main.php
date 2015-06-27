3f716a78a2909b246a7d09f1d5b608cd9277b272
Benjamin Niess <benjamin.niess@gmail.com>
2015-06-27 22:18:52 +1000

    Keeped old swf embed code

--- classes/main.php
+++ classes/main.php
@@ -11,16 +11,17 @@ class IPS_Main {
 
 		if ( empty( $ips_options ) ) {
 			update_option( 'ips_options', array(
-				'allow_full_screen' => 1,
-				'auto_upload'       => 1,
-				'add_ips_button'    => 1,
-				'width'             => 640,
-				'height'            => 480,
-				'layout'            => 1,
-				'autoflip'          => 0,
-				'show_flip_buttons' => 0,
-				'bgcolor'           => 'FFFFFF',
-				'flip_timelaps'     => 6000,
+				'allow_full_screen'	=> 1,
+				'auto_upload'		=> 1,
+				'add_ips_button'	=> 1,
+				'width'				=> 640,
+				'height'			=> 480,
+				'layout'			=> 1,
+				'autoflip'			=> 0,
+				'show_flip_buttons'	=> 0,
+				'bgcolor'			=> 'FFFFFF',
+				'flip_timelaps'		=> 6000,
+				'api_version'		=> 'new',
 			) );
 		}
 	}
--- classes/shortcodes.php
+++ classes/shortcodes.php
@@ -11,6 +11,14 @@ class IPS_Shortcodes {
 	public static function issuu_pdf_embeder($atts, $content = null) {
 		global $ips_options;
 
+		if ( isset( $ips_options['api_version'] ) && 'new' == $ips_options['api_version'] ) {
+			return self::shortcode_new_api( $atts );
+		} else {
+			return self::shortcode_old_api( $atts );
+		}
+	}
+
+	public static function shortcode_old_api( $atts = array() ) {
 		if ( isset( $ips_options['layout'] ) && 2 == $ips_options['layout'] ) {
 			$layout = 'presentation';
 		} else {
@@ -46,6 +54,73 @@ class IPS_Shortcodes {
 			'autoFlip' => $autoFlip,
 			'loadingInfoText' => __( 'Loading...', 'ips' ),
 		);
+
+		if ( isset( $customLayout ) && ! empty( $customLayout ) ) {
+			if ( is_dir( TEMPLATEPATH . '/issuu-skins/' . $customLayout ) ) {
+				$parameters['layout'] = get_bloginfo( 'template_directory' ) . '/issuu-skins/' . $customLayout . '/layout.xml';
+
+				$layout_embed = ' layout="' . ( get_bloginfo( 'template_directory' ) . '/issuu-skins/' . $customLayout . '/layout.xml' ) . '"';
+			} else {
+				$parameters['layout'] = IPS_URL . '/issuu-skins/' . $customLayout . '/layout.xml';
+
+				$layout_embed = ' layout="' . ( IPS_URL . '/issuu-skins/' . $customLayout . '/layout.xml' ) . '"';
+			}
+		} else {
+			$layout_url   = '';
+			$layout_embed = '';
+		}
+
+		$issuu_swf_url = 'http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf';
+		$issuu_swf_url = add_query_arg( $parameters, $issuu_swf_url );
+
+		$flashvars = build_query( $parameters );
+
+		// Dimensions
+		if ( strpos( $width, 'px' ) === false && strpos( $width, '%' ) === false ) {
+			$width .= 'px';
+		}
+
+		if ( strpos( $height, 'px' ) === false && strpos( $height, '%' ) === false ) {
+			$height .= 'px';
+		}
+
+		$style = sprintf( 'width: %s; height: %s', $width, $height );
+
+		// Start to get the content to return it at the end
+		ob_start(); ?>
+
+		<div>
+			<object style="<?php echo esc_attr( $style ); ?>" >
+				<param name="movie" value="<?php echo esc_attr( $issuu_swf_url ); ?>" />
+				<param name="allowfullscreen" value="<?php echo $allowfullscreen; ?>" />
+				<param name="wmode" value="transparent" />
+				<param name="menu" value="false" />
+				<embed src="http://static.issuu.com/webembed/viewers/style1/v1/IssuuViewer.swf" <?php echo $layout_embed; ?> type="application/x-shockwave-flash" allowfullscreen="<?php echo $allowfullscreen; ?>" wmode="transparent" menu="false" style="<?php echo esc_attr( $style ); ?>" flashvars="<?php echo esc_attr( $flashvars ); ?>" />
+			</object>
+		</div>
+
+		<?php do_action( 'after-ips-shortcode', $issuu_pdf_id ); ?>
+
+		<?php
+
+		// Return the shortcode content
+		$content = ob_get_contents();
+		ob_end_clean();
+
+		return $content;
+	}
+
+	public static function shortcode_new_api( $atts = array() ) {
+		extract(shortcode_atts( array(
+			'issuu_pdf_id' => null,
+			'width' => $ips_options['width'],
+			'height' => $ips_options['height'],
+		), $atts ));
+
+		// Check if the required param is set
+		if ( empty( $issuu_pdf_id ) ) {
+			return false;
+		}
 
 		$issuu_api = new IPS_Issuu_Api();
 		if ( ! $issuu_api->is() ) {
